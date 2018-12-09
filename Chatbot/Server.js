52bab3f9ce68a7a67bfa5209cf1fc8f4e9d44bb2
lihaojin <hli002@citymail.cuny.edu>
2018-12-08 19:54:42 -0500

    replaced workspace id

--- Chatbot/Server.js
+++ Chatbot/Server.js
@@ -3,18 +3,20 @@ var express = require('express'),
     path = require("path"),
     fs = require("fs");
 
+var fs = require('fs');
 var AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
-    var Chatbot = function ( ) {
-      var assistant = new AssistantV1({
-        version: '2018-09-20',
-        iam_apikey: '0Cj8Uhwr7l9nAZz9XxP7jS7nfLpdcJWpBu14UAe5jGyn',
-        url: 'https://gateway.watsonplatform.net/assistant/api'
-      });
 
-      var workspace_id = 'e4653a50-482f-40c4-b366-8110899a728e';
+ var Chatbot = function (){
+   var assistant = new AssistantV1({
+     version: '2018-09-20',
+     iam_apikey: '0Cj8Uhwr7l9nAZz9XxP7jS7nfLpdcJWpBu14UAe5jGyn',
+     url: 'https://gateway.watsonplatform.net/assistant/api'
+   });
 
-      this.sendMessage = function (msg) {
+   var workspace_id = 'bff1a124-a94a-4082-a762-4a38e7f6d4b7';
+
+   this.sendMessage = function(msg) {
         assistant.message({
           workspace_id: workspace_id,
           input: {'text': msg}
@@ -24,12 +26,18 @@ var AssistantV1 = require('watson-developer-cloud/assistant/v1');
           else
             console.log(JSON.stringify(response, null, 2));
         });
-      };
-    };
+     };
+};
 
 
 const _port = 3000;
 var app = express();
-var investopal = Chatbot();
+var investo = new Chatbot();
+
+// app.use(express.static(path.join(__dirname, "../agora.css")));
+// app.use(express.static(path.join(__dirname, "../agora.js")));
+// app.get("/",function(req,res){
+//      res.sendFile(path.join(__dirname, "../agora.html"));
+// });
 
 app.listen(_port, function() { console.log('listening port '+_port+"\n__dirname : "+__dirname)});
